285fab4eb23a9cee5f535841459aafe4c69b0f64
Guangqing Tang <guangqing@laioffer.com>
2018-07-12 00:22:23 -0700

    queue with iterator2

--- aggFolder.py
+++ aggFolder.py
@@ -4,8 +4,8 @@ import itertools
 from collections import deque
 
 # take input from user
-#input = input('Please Enter the absolute path for a directory: ')
-input = '/Users/root1/Documents/unixstuff'
+input = input('Please Enter the absolute path for a directory: ')
+# input = '/Users/root1/Documents/unixstuff'
 
 # dictionary to store modification times
 # maxOfM: [current most frequent modified time, track #times]
@@ -22,36 +22,33 @@ maxOfE = [None, 0]
 # if folder: recursion
 # if file: store extension in dicExt
 def fileHandler(filePath):
-    try:
-        if os.path.exists(filePath):
-            # handle modification times
-            statbuf = os.stat(filePath)
-            mtime = time.strftime('%m/%d/%Y', time.gmtime(statbuf.st_mtime))
-            if mtime in dicTime:
-                dicTime[mtime] += 1
-            else:
-                dicTime[mtime] = 1
-            # update maximum
-            if maxOfM[1] < dicTime[mtime]:
-                maxOfM[0] = mtime
-                maxOfM[1] = dicTime[mtime]
+    print('File Included: {}'.format(filePath))
+    if os.path.exists(filePath):
+        # handle modification times
+        statbuf = os.stat(filePath)
+        mtime = time.strftime('%m/%d/%Y', time.gmtime(statbuf.st_mtime))
+        if mtime in dicTime:
+            dicTime[mtime] += 1
+        else:
+            dicTime[mtime] = 1
+        # update maximum
+        if maxOfM[1] < dicTime[mtime]:
+            maxOfM[0] = mtime
+            maxOfM[1] = dicTime[mtime]
 
-            # handle extensions
-            if not os.path.isdir(filePath):
-                extension = os.path.splitext(filePath)[-1]
-                if extension is not '':
-                    if extension in dicExt:
-                        dicExt[extension] += 1
-                    else:
-                        dicExt[extension] = 1
-                    # update maximum
-                    if maxOfE[1] < dicExt[extension]:
-                        maxOfE[0] = extension
-                        maxOfE[1] = dicExt[extension]
+        # handle extensions
+        if not os.path.isdir(filePath):
+            extension = os.path.splitext(filePath)[-1]
+            if extension is not '':
+                if extension in dicExt:
+                    dicExt[extension] += 1
+                else:
+                    dicExt[extension] = 1
+                # update maximum
+                if maxOfE[1] < dicExt[extension]:
+                    maxOfE[0] = extension
+                    maxOfE[1] = dicExt[extension]
 
-
-    except (FileNotFoundError, OSError, PermissionError):
-        print('File Not Included {}'.format(filePath))
 
 
 # walk through given path
@@ -63,17 +60,18 @@ while queue:
         curfolder = next(queue)
     except StopIteration:
         break
-    dirs = []
-    try:
-        for file in os.scandir(curfolder):
-            fileHandler(os.path.join(curfolder, file))
-            if os.path.isdir(file):
-                dirs.append(os.path.join(curfolder, file))
-    except (FileNotFoundError, OSError, PermissionError):
-        print('File Not Included {}'.format(curfolder))
-    if dirs:
-        # chain the sub-level folders to queue
-        queue = itertools.chain(queue, dirs)
+    if os.path.exists(curfolder):
+        dirs = []
+        try:
+            for file in os.scandir(curfolder):
+                fileHandler(os.path.join(curfolder, file))
+                if os.path.isdir(file):
+                    dirs.append(os.path.join(curfolder, file))
+        except (FileNotFoundError, OSError, PermissionError):
+            print('File Not Included: {}'.format(curfolder))
+        if dirs:
+            # chain the sub-level folders to queue
+            queue = itertools.chain(queue, dirs)
 
 
 
